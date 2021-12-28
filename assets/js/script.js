7dd24f91ebe1c2ab5ddad150299537dda4c42843
ftapioca <ftapioca@gmail.com>
2021-12-28 17:33:26 -0300

    factorial listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,26 +7,25 @@ var num_mult = prompt("Bienvenido al desafío de las multiplicaciones, ingresa u
 let multiplicar = (mult) => {
     //Confirmación de número entre 1 y 20
     if (mult > 20) {
-        console.log("estamos aqui");
         alert("El número ingresado es mayor a 20, debes ingresar un número entre 1 y 20");
     } else if(mult < 1) {
         alert("El número ingresado es menor a 1, debes ingresar un número entre 1 y 20");
     } else { 
-        
-        
         for (let i = 1 ; i <= mult ; i++){
             var b = mult*i;
             console.log(i + " * " + mult + " = "+ b);
         }
-    
+        //formula para factorial
+        for (let i = 1; i <= mult; i++){
+            let res = 1;
+            for (let j = 1; j <= i; j++){
+                res = res*j;
+            }
+            console.log("Factorial de " + i + " es: " + res);
+        }
     }
-    //formula para factorial
-    /* 
-    if (mult > 1){
-        return mult * multiplicar(mult-1) 
-    } else {
-        return mult;
-    }*/
+    
+
 } 
 
 multiplicar(num_mult);
